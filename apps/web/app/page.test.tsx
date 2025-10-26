7fef8a37354b24a59ec8d22555fac4cdb121d569
eduesplinio <eduesplinio.dat@gmail.com>
2025-10-26 10:29:21 -0300

    fix(test): remove obselete page `test`

--- apps/web/app/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Page from './page';
-
-describe('Home Page', () => {
-  it('should render successfully', () => {
-    render(<Page />);
-    expect(screen.getByText('Hello World')).toBeInTheDocument();
-  });
-});
